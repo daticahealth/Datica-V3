88e9467874663969eef5b6ff72bcb46e4cb192bf
R. Allan White <allan@catalyze.io>
2019-11-01 17:49:50 -0700

    Events, tailwind config, components

--- public/site/themes/main/tailwind.config.js
+++ public/site/themes/main/tailwind.config.js
@@ -79,7 +79,7 @@ module.exports = {
       // thin: '200',
       light: '300',
       normal: '400',
-      // medium: '500',
+      medium: '500',
       // semibold: '600',
       bold: '700',
       // extrabold: '800',
